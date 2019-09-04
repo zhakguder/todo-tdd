fc9be91c068231c4a80c03610a7f5135ea653ac3
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2019-09-04 17:28:02 -0500

    resolve home route

--- django_server/lists/tests.py
+++ django_server/lists/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 
+from django.urls import resolve
+from lists.views import home_page
+
 # Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(3+1, 6)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_homepage(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
