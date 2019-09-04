c84543645c11806fb9aefd93459e87b4aa63e72c
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2019-09-04 17:17:35 -0500

    Add app for lists

---
+++ django_server/functional_tests.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser =  webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        browser.get('http://localhost:8000')
+        self.assertIsIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+
+if __name__ == "__main__":
+    unittest.main()
